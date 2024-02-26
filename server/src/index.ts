81aecaccb2e4a4348d69bd3077a7bbd5e2701ae4
Jonas Souza <Jonassouza305@yahoo.com.br>
2024-02-25 22:55:45 -0300

    adicionei o pacote `cors` no servidor

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,10 @@
 import express from "express";
 import database from "../infra/database";
-
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(cors());
 
 function transformTheFirstLetterOfThePhraseIntoUppercase(text: string) {
   return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
